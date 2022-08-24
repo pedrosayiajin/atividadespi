f45eeda4cd3eb1b7672b7fd1cc54b81c6c6fe260
Pedro Gustavo Dantas da Cunha de Lima <gustavo.d@academico.ifrn.edu.br>
2022-08-24 17:21:23 -0300

    Colocando os arquivos iniciais na minha aplicação

---
+++ src/main/java/atividades/pi/atividades/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package atividades.pi.atividades.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Chamou o metódo");
+		return "home";
+	}
+}
---
+++ src/test/java/atividades/pi/atividades/AtividadesApplicationTests.java
@@ -0,0 +1,13 @@
+package atividades.pi.atividades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
