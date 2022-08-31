5431dfe1d9f8fefd439c2a278440dd8527a2a3af
Pedro Gustavo Dantas da Cunha de Lima <gustavo.d@academico.ifrn.edu.br>
2022-08-31 16:26:05 -0300

    Criando modelo para Evento

--- src/main/java/atividades/pi/atividades/models/Evento.java
+++ src/main/java/atividades/pi/atividades/models/Evento.java
@@ -1,5 +1,35 @@
 package atividades.pi.atividades.models;
 
 public class Evento {
+	
+	private String nome;
+	private String data;
+	private String local;
+	private String horário;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getHorário() {
+		return horário;
+	}
+	public void setHorário(String horário) {
+		this.horário = horário;
+	}
 
 }
