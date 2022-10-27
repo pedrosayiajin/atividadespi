6c94aac70f1e47d3fe44020fcc370972b7198347
Pedro Gustavo Dantas da Cunha de Lima <gustavo.d@academico.ifrn.edu.br>
2022-10-27 14:04:57 -0300

    detalhando eventos

--- src/main/java/atividades/pi/atividades/controllers/EventosController.java
+++ src/main/java/atividades/pi/atividades/controllers/EventosController.java
@@ -1,13 +1,14 @@
 package atividades.pi.atividades.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import atividades.pi.atividades.models.Evento;
@@ -20,28 +21,45 @@ public class EventosController {
 	@Autowired
 	private EventoRepository er;
 	
-	@RequestMapping ("/eventos/form")
+	@RequestMapping ("/form")
 	public String form() {
-		return "formEvento";
+		return "eventos/formEvento";
 	}
 
-	@GetMapping
+	@PostMapping
 	public String adicionar(Evento evento) {
 		
 		System.out.println(evento);
 		er.save(evento);
 		
-		return "evento-adicionado";
+		return "eventos/evento-adicionado";
 	}
 	
-	@GetMapping("/eventos")
+	@GetMapping
 	public ModelAndView listar(){
 		
 		List<Evento> eventos = er.findAll();
-		ModelAndView mv = new ModelAndView("/eventos/form");
+		ModelAndView mv = new ModelAndView("eventos/lista");
 		mv.addObject("eventos", eventos);
 		return mv;
 	}
 	
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Evento> opt = er.findById(id);
+		if(opt.isEmpty()) {
+			md.setViewName("redirect:/eventos");
+			return md;
+		}
+		
+		md.setViewName("eventos/detalhes");
+		Evento evento = opt.get();
+	    
+		md.addObject("evento", evento);
+		return md;
+		
+		
+	}
 	
 }
